e59113a620c525b6782ba2bed82385c3f6815d5e
Jesus Hernandez Dimas <jesus.dimas@neoris.cxnetworks.net>
2018-10-11 14:14:28 -0600

    test

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-//test s
+//test s d
 namespace netcore_template_react
 {
     public class Program
