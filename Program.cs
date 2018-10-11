b77950bccb86a9dc34ec707997503c3dbebf5c32
Jesus Hernandez Dimas <jesus.dimas@neoris.cxnetworks.net>
2018-10-11 13:00:12 -0600

    tercer mensaje

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-//test
+//test s
 namespace netcore_template_react
 {
     public class Program
